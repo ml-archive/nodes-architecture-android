55bde7b168a7d64fb82a99c69f070566dfb8f342
Johnny Sørensen <joso@nodes.dk>
2017-11-13 16:37:47 +0100

    Merge branch 'master' of github.com:nodes-android/nodes-architecture-android
    # Conflicts:
    #	app/src/main/java/dk/nodes/arch/presentation/mvp/MvpBasePresenter.kt

--- app/src/main/java/dk/nodes/arch/presentation/base/BasePresenterImpl.kt
+++ app/src/main/java/dk/nodes/arch/presentation/base/BasePresenterImpl.kt
@@ -3,8 +3,11 @@ package dk.nodes.arch.presentation.base
 import android.arch.lifecycle.Lifecycle
 import android.arch.lifecycle.LifecycleObserver
 import android.arch.lifecycle.OnLifecycleEvent
+import dk.nodes.arch.presentation.mvp.ViewAction
+import java.util.concurrent.LinkedBlockingQueue
 
 abstract class BasePresenterImpl<V> : BasePresenter<V>, LifecycleObserver {
+    private val cachedViewActions = LinkedBlockingQueue<ViewAction<V>>()
     protected var view: V? = null
     protected var lifecycle: Lifecycle? = null
 
@@ -22,7 +25,9 @@ abstract class BasePresenterImpl<V> : BasePresenter<V>, LifecycleObserver {
 
     @OnLifecycleEvent(Lifecycle.Event.ON_RESUME)
     override fun onResume() {
-
+        while (!cachedViewActions.isEmpty() && view != null) {
+            view?.let { cachedViewActions.poll().run(it) }
+        }
     }
 
     @OnLifecycleEvent(Lifecycle.Event.ON_PAUSE)
@@ -37,6 +42,17 @@ abstract class BasePresenterImpl<V> : BasePresenter<V>, LifecycleObserver {
     @OnLifecycleEvent(Lifecycle.Event.ON_DESTROY)
     override fun onViewDetached() {
         view = null
+        cachedViewActions?.clear()
         lifecycle?.removeObserver(this)
+    }
+
+    fun run(action: ViewAction<V>) {
+        if (view != null) {
+            view?.let {
+                action.run(it)
+            }
+        } else {
+            cachedViewActions.add(action)
+        }
     }
 }
