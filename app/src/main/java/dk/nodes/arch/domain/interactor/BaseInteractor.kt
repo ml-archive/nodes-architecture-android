d3ece0d0a07a1dd417b792052fe54377687657b2
Stinus Petersen <stpe@nodes.dk1>
2017-12-19 14:36:15 +0100

    Added uncaught exception handling to interactors with support for hockeyapp crash upload (if the hockeyapp sdk is loaded in the client app)

--- app/src/main/java/dk/nodes/arch/domain/interactor/BaseInteractor.kt
+++ app/src/main/java/dk/nodes/arch/domain/interactor/BaseInteractor.kt
@@ -1,5 +1,6 @@
 package dk.nodes.arch.domain.interactor
 
+import android.util.Log
 import dk.nodes.arch.domain.executor.Executor
 /**
  * Created by bison on 24-06-2017.
@@ -8,7 +9,15 @@ import dk.nodes.arch.domain.executor.Executor
 abstract class BaseInteractor(protected val executor: Executor) : Interactor {
     override fun run() {
         executor.execute(Runnable {
-            execute()
+            try {
+                execute()
+            }
+            catch (t : Throwable)
+            {
+                //Log.e("BaseInteractor", "Uncaught throwable in thread ${Thread.currentThread()?.name}")
+                //t.printStackTrace()
+                submitToHockey(t)
+            }
         })
     }
 
@@ -18,4 +27,24 @@ abstract class BaseInteractor(protected val executor: Executor) : Interactor {
     }
 
     abstract fun execute()
+
+    fun submitToHockey(t : Throwable)
+    {
+        val exceptionHandlerCls = Class.forName("net.hockeyapp.android.ExceptionHandler")
+        if(exceptionHandlerCls == null)
+        {
+            Log.e("BaseInteractor", "Could not load HockeyApp SDK Classes, cannot record crash")
+        }
+
+        try {
+            val default_handler = exceptionHandlerCls.cast(Thread.getDefaultUncaughtExceptionHandler())
+            val method = exceptionHandlerCls.getMethod("uncaughtException", Thread::class.java, Throwable::class.java)
+            method.invoke(default_handler, Thread.currentThread(), t)
+        }
+        catch(e : ClassCastException)
+        {
+            e.printStackTrace()
+            Log.e("BaseInteractor", "Could not get HockeySDK uncaught exception handler")
+        }
+    }
 }
