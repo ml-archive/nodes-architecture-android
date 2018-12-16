a1631ef2985a2959b3deb31bdca9468fa2bf5360
lucassales <saless182@gmail.com>
2018-12-16 15:36:33 -0200

    Adding result interactor

--- arch/src/main/java/dk/nodes/arch/domain/interactor/Interactor.kt
+++ arch/src/main/java/dk/nodes/arch/domain/interactor/Interactor.kt
@@ -10,9 +10,27 @@ interface Interactor<in P> {
     suspend operator fun invoke(executeParams: P)
 }
 
+interface ResultInteractor<in P, out R : Any> {
+    val dispatcher: CoroutineDispatcher
+    suspend operator fun invoke(executeParams: P, onResult: (Result<R>) -> Unit)
+}
+
 fun <P> CoroutineScope.launchInteractor(interactor: Interactor<P>, param: P): Job {
     return launch(context = interactor.dispatcher, block = { interactor(param) })
 }
 
 fun CoroutineScope.launchInteractor(interactor: Interactor<Unit>) =
     launchInteractor(interactor, Unit)
+
+fun <P, R : Any> CoroutineScope.launchInteractor(
+    interactor: ResultInteractor<P, R>,
+    param: P,
+    onResult: (Result<R>) -> Unit
+): Job {
+    return launch(context = interactor.dispatcher, block = { interactor(param, onResult) })
+}
+
+fun <R : Any> CoroutineScope.launchInteractor(
+    interactor: ResultInteractor<Unit, R>,
+    onResult: (Result<R>) -> Unit
+) = launchInteractor(interactor, Unit, onResult)
