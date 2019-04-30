5fc7577110d2cef04c1bf124bf7a9d0333df575e
lucassales <saless182@gmail.com>
2019-04-30 12:38:40 +0200

    Adding deprecated methods

--- app/src/main/java/dk/nodes/arch/presentation/base/BasePresenter.kt
+++ app/src/main/java/dk/nodes/arch/presentation/base/BasePresenter.kt
@@ -1,9 +1,22 @@
 package dk.nodes.arch.presentation.base
 
+import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.LifecycleOwner
 import kotlin.coroutines.CoroutineContext
 
 interface BasePresenter<in V> {
+
+    @Deprecated(
+        "For fragments use Fragment.getViewLifecycleOwner()",
+        replaceWith = ReplaceWith("onCreate(view: V, lifecycleOwner: LifecycleOwner)")
+    )
+    fun onCreate(view: V, lifecycle: Lifecycle)
+
+    @Deprecated(
+        "For fragments use Fragment.getViewLifecycleOwner()",
+        replaceWith = ReplaceWith("onViewCreated(view: V, lifecycleOwner: LifecycleOwner)")
+    )
+    fun onViewCreated(view: V, lifecycle: Lifecycle)
 
     fun onCreate(view: V, lifecycleOwner: LifecycleOwner)
 
