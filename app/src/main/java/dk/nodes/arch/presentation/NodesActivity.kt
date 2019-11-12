2ee98685c554e44ce56721d74959ce558cca341b
lucassales <saless182@gmail.com>
2019-11-12 11:21:53 +0100

    Alpha 5

--- app/src/main/java/dk/nodes/arch/presentation/NodesActivity.kt
+++ app/src/main/java/dk/nodes/arch/presentation/NodesActivity.kt
@@ -3,16 +3,13 @@ package dk.nodes.arch.presentation
 import android.content.Context
 import android.os.Bundle
 import androidx.activity.viewModels
-import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
-import dagger.android.AndroidInjection
-import dagger.android.DispatchingAndroidInjector
-import dagger.android.HasAndroidInjector
+import dagger.android.support.DaggerAppCompatActivity
 import dk.nodes.nstack.kotlin.inflater.NStackBaseContext
 import javax.inject.Inject
 
-abstract class NodesActivity : AppCompatActivity, HasAndroidInjector {
+abstract class NodesActivity : DaggerAppCompatActivity {
 
     constructor(contentResId: Int) : super() {
         this.contentResId = contentResId
@@ -26,15 +23,10 @@ abstract class NodesActivity : AppCompatActivity, HasAndroidInjector {
     protected inline fun <reified VM : ViewModel> viewModel(): Lazy<VM> =
         viewModels { viewModelFactory }
 
-    @Inject internal lateinit var injector: DispatchingAndroidInjector<Any>
-
     override fun onCreate(savedInstanceState: Bundle?) {
-        AndroidInjection.inject(this)
-        contentResId?.let(::setContentView)
         super.onCreate(savedInstanceState)
+        contentResId?.let(::setContentView)
     }
-
-    override fun androidInjector() = injector
 
     override fun attachBaseContext(newBase: Context) {
         super.attachBaseContext(NStackBaseContext(newBase))
